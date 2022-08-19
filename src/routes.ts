588098c6c1ebf2701b75922c2a40daafd3c9c7cc
lucasroquesousa <lucasroquesousa@hotmail.com>
2022-08-19 09:47:35 -0300

    feat: criando configuração de erro e instalando o cors para qque qualquer ip possa acessar

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,10 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
+//localhost:3333/teste
+
 router.get("/teste", (req: Request, res: Response) => {
-  return res.json({ ok: true });
+  return res.json({ nome: "Lucas" });
 });
 
 export { router };
